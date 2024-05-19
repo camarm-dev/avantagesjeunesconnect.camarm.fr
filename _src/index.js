d4d74cd6cf93a17af0b0c13ee88278606537f8df
camarm-dev <contact@camarm.dev>
2024-05-19 16:17:52 +0200

    fix: removed dark mode

--- _src/index.js
+++ _src/index.js
@@ -6,8 +6,8 @@ const tobi = new Tobi()
 
 // Remove the two following lines to remove the product hunt floating prompt
 // Remove the following lines to remove the darkmode js
-import Darkmode from 'darkmode-js'
-function addDarkmodeWidget() {
-  new Darkmode().showWidget()
-}
-window.addEventListener('load', addDarkmodeWidget)
+// import Darkmode from 'darkmode-js'
+// function addDarkmodeWidget() {
+//   new Darkmode().showWidget()
+// }
+// window.addEventListener('load', addDarkmodeWidget)
